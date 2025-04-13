2c572bc12f66319e800e648a6798cbffe083980d
talha aksoy <talha-_-0@hotmail.com>
2025-04-13 21:18:08 +0300

    fix: optional name extract fix

--- src/functions/get_template_from_repo.rs
+++ src/functions/get_template_from_repo.rs
@@ -1,13 +1,17 @@
-use reqwest::Client;
 use flate2::read::GzDecoder;
+use reqwest::Client;
+use std::fs;
+use std::path::{Path, PathBuf};
 use tar::Archive;
 
-pub async fn get_template_from_repo(template_id: &str , folder_name : Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn get_template_from_repo(
+    template_id: &str,
+    folder_name: Option<&str>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let template_name: Vec<&str> = template_id.split('@').collect();
 
     if template_name.len() != 2 {
-        return Err("❌ Invalid template identifier format. Use something like 'react@ts'"
-            .into());
+        return Err("❌ Invalid template identifier format. Use something like 'react@ts'".into());
     }
 
     let filename = format!("{}-{}.tar.gz", template_name[0], template_name[1]);
@@ -24,13 +28,28 @@ pub async fn get_template_from_repo(template_id: &str , folder_name : Option<&st
     }
 
     let bytes = response.bytes().await?;
-
-    let tar_gz = GzDecoder::new(&bytes[..]); // [u8] slice
+    let tar_gz = GzDecoder::new(&bytes[..]);
     let mut archive = Archive::new(tar_gz);
 
-    let target_dir = folder_name.unwrap_or(".");
-    archive.unpack(target_dir)?;
+    let target_dir = Path::new(folder_name.unwrap_or("template"));
 
-    println!("✅ Template unpacked successfully.");
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let path = entry.path()?;
+        let stripped_path = path
+            .iter()
+            .skip(1) // İlk klasörü atla (orijinal arşivdeki klasör ismini)
+            .collect::<PathBuf>();
+
+        let dest_path = target_dir.join(stripped_path);
+
+        if let Some(parent) = dest_path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        entry.unpack(&dest_path)?;
+    }
+
+    println!("✅ Template unpacked into: {:?}", target_dir);
     Ok(())
 }
